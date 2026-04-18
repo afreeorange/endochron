c33049daab1f7ab75dbaa7b4768e40e1144599fa
Nikhil Anand <mail@nikhil.io>
2026-04-17 23:36:38 -0400

    Lol

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -18,22 +18,42 @@ export const Shell = ({ children, disableDock }: ShellProps) => {
       {children}
 
       <div className="dock dock-md">
-        <button disabled={disableDock} className={clsx(pathname.includes("/speak") && "dock-active")}>
+        <button
+          disabled={disableDock}
+          className={clsx(
+            pathname.includes("/record") ? "dock-active" : "opacity-35",
+          )}
+        >
           <PiMicrophoneDuotone className="text-2xl" />
           <span className="dock-label">Record</span>
         </button>
 
-        <button disabled={disableDock} className={clsx(pathname.includes("/reflect") && "dock-active")}>
+        <button
+          disabled={disableDock}
+          className={clsx(
+            pathname.includes("/reflect") ? "dock-active" : "opacity-35",
+          )}
+        >
           <PiAsclepiusDuotone className="text-2xl" />
           <span className="dock-label">Reflect</span>
         </button>
 
-        <button disabled={disableDock} className={clsx(pathname.includes("/prepare") && "dock-active")}>
+        <button
+          disabled={disableDock}
+          className={clsx(
+            pathname.includes("/prepare") ? "dock-active" : "opacity-35",
+          )}
+        >
           <PiPersonDuotone className="text-2xl" />
           <span className="dock-label">Prepare</span>
         </button>
 
-        <button disabled={disableDock} className={clsx(pathname.includes("/settings") && "dock-active")}>
+        <button
+          disabled={disableDock}
+          className={clsx(
+            pathname.includes("/settings") ? "dock-active" : "opacity-35",
+          )}
+        >
           <PiGearDuotone className="text-2xl" />
           <span className="dock-label">Settings</span>
         </button>
